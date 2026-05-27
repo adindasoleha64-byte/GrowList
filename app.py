66316b4398a8787e2390047191943b422cc51b66
Adinda Soleha <adindasoleha64@gmail.com>
2026-05-27 22:14:16 +0700

    fix railway port env 1.1

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, jsonify, request
 from tree import TaskNode
-
+import os
 app = Flask(__name__)
 
 # Dummy data
@@ -26,4 +26,5 @@ def add_task():
     return jsonify({"status": "success"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
